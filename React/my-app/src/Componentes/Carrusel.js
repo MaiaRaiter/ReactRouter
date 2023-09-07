a4acce1df48fa92b2b0de97652e39023ad84ddb8
SHIRLY JUSID <46875521@ort>
2023-09-07 10:40:29 -0300

    carrousel

--- React/my-app/src/Componentes/Carrusel.js
+++ React/my-app/src/Componentes/Carrusel.js
@@ -1,27 +1,27 @@
-/*import React from 'react'
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 
+export default function Carrusel () {
 
-export const Carrusel = () => {
     return (
-        <Carousel>
-            <section id="billboard" className="overflow-hidden">
+        
+           <Carousel>
+           
 
-                <button className="button-prev">
-                    <i className="icon icon-chevron-left"></i>
-                </button>
-                <button className="button-next">
-                    <i className="icon icon-chevron-right"></i>
-                </button>
-                <div className="swiper main-swiper">
-                    <div className="swiper-wrapper">
-                        <div className="swiper-slide Banner1" >
+                        <div className="main-swiper swiper-wrapper swiper-slide Banner1" >
+                            <img src="images/banner1.jpg" className="product-image" />
+
                             <div className="banner-content">
+
                                 <div className="container">
+
                                     <div className="row">
                                         <div className="col-md-6">
+                                            <div className="image-holder">
+
+                                            </div>
                                             <h2 className="banner-title">Summer Collection</h2>
                                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu feugiat amet, libero ipsum enim pharetra hac.</p>
 
@@ -30,8 +30,9 @@ export const Carrusel = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="swiper-slide Banner2" >
-
+                        
+                        <div className="main-swiper swiper-wrapper  Banner2" >
+                        <img src="images/banner2.jpg" className="product-image" />
                             <div className="banner-content">
                                 <div className="container">
                                     <div className="row">
@@ -39,20 +40,22 @@ export const Carrusel = () => {
                                             <h2 className="banner-title">Casual Collection</h2>
                                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu feugiat amet, libero ipsum enim pharetra hac.</p>
 
-                                        </div>
+                                        
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <script defer src="Carrusel.js"></script>
-
-            </section>
-        </Carousel>
+                        </div>
+             
 
 
-    )
+
+                  
+               
+      
+             </Carousel>
+       
+   )
 }
-ReactDOM.render(<Carrusel />, document.querySelector('.demo-carousel'));
-*/
+
+
--- React/my-app/src/Componentes/pages/Home.js
+++ React/my-app/src/Componentes/pages/Home.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Footer from '../Footer'
 import Header from '../Header'
-import { Carrusel } from '../Carrusel'
+import "react-responsive-carousel/lib/styles/carousel.min.css" // requires a loader
+import  Carousel  from '../Carrusel'
 import Producto from '../Producto'
 
 export default function Home() {
@@ -45,56 +46,12 @@ export default function Home() {
         </div>
 
         < Header />
-
-        <section id="billboard" className="overflow-hidden">
-
-          <button className="button-prev">
-            <i className="icon icon-chevron-left"></i>
-          </button>
-          <button className="button-next">
-            <i className="icon icon-chevron-right"></i>
-          </button>
-          <div className="swiper main-swiper">
-          
-            <div className="swiper-wrapper">
-            
-              <div className="swiper-slide Banner1" >
-              <img src="images/banner1.jpg"  className="product-image"/>
-                <div className="banner-content">
-                
-                  <div className="container">
-                  
-                    <div className="row">
-                      <div className="col-md-6">
-                      <div className="image-holder">
-                        
-                      </div>
-                        <h2 className="banner-title">Summer Collection</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu feugiat amet, libero ipsum enim pharetra hac.</p>
-
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="swiper-slide Banner2" >
-
-                <div className="banner-content">
-                  <div className="container">
-                    <div className="row">
-                      <div className="col-md-6">
-                        <h2 className="banner-title">Casual Collection</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu feugiat amet, libero ipsum enim pharetra hac.</p>
-
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-        </section>
+      
+       
+             
+      
+   
+        <Carousel/>
 
         <section id="featured-products" className="product-store padding-large">
           <div className="container">
