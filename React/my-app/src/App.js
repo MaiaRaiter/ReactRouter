71e0688e4913577609fd1b09e426b9f2bfd1c1a6
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-12 10:40:04 -0300

    Detalle Productos

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -2,6 +2,7 @@ import Home from './Componentes/pages/Home';
 import { Route, Routes } from 'react-router-dom';
 import Productos from './Componentes/pages/Productos';
 import { Contacto } from './Componentes/pages/Contacto';
+import DetalleProductos from './Componentes/pages/DetalleProductos'
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
      <Route path='/' element={<Home />}> </Route>
      <Route path='/Productos' element={<Productos />}> </Route>
      <Route path='/Contacto' element={<Contacto />}> </Route>
+     <Route path='/DetalleProductos' element={<DetalleProductos />}> </Route>
+    
     
     
    </Routes>
