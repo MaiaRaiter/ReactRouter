82bd87cd8707125a461308f29c56f99b71812ad7
ShirJu <108415419+ShirJu@users.noreply.github.com>
2023-09-24 00:09:43 -0300

    .

--- React/my-app/src/Componentes/Carrusel.js
+++ React/my-app/src/Componentes/Carrusel.js
@@ -8,7 +8,7 @@ export default function Carrusel() {
 
         <Carousel>
             <div className="main-swiper swiper-wrapper swiper-slide Banner1" >
-                <img src="images/banner1.jpg" className="product-image" />
+                <img src=" https://i.dummyjson.com/data/products/1/thumbnail.jpg" className="product-image" />
 
                 <div className="banner-content">
 
@@ -19,7 +19,7 @@ export default function Carrusel() {
                                 <div className="image-holder">
 
                                 </div>
-                                <h2 className="banner-title">Summer Collection</h2>
+                                <h2 className="banner-title">Home</h2>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu feugiat amet, libero ipsum enim pharetra hac.</p>
 
                             </div>
@@ -29,7 +29,7 @@ export default function Carrusel() {
             </div>
 
             <div className="main-swiper swiper-wrapper  Banner2" >
-                <img src="images/banner2.jpg" className="product-image" />
+                <img src=" https://i.dummyjson.com/data/products/4/thumbnail.jpg" className="product-image" />
                 <div className="banner-content">
                     <div className="container">
                         <div className="row">
